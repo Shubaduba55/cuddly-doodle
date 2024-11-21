8dd5a6d742124b2b55a9205c7dd02ec716a14881
Bovvan <shamkov2323@gmail.com>
2024-11-21 22:27:18 +0200

    feat: add input data read, image displayed
    Inserted data is read: text, radiobutton and image file.
    Image file is displayed below the form.

--- js/script.js
+++ js/script.js
@@ -4,43 +4,82 @@ function clearAllFields(){
     document.getElementById('song-form').reset();
 }
 
-function actionButtonSubmit(){
+function actionButtonSubmit(uploader){
     // General Info
-    songUrl = document.getElementById('song-url');
-    songName = document.getElementById('song-name');
-    bandName = document.getElementById('band-name');
+    const songUrl = document.getElementById('input-song-url').value;
+    const songName = document.getElementById('input-song-name').value;
+    const bandName = document.getElementById('input-band-name').value;
 
     // Your impression
-    songAdjectives = document.getElementById('song-adjectives');
-    songMood = document.getElementById('song-mood');
-    momentInLife = document.getElementById('moment-in-life');
-    associations = document.getElementById('associations');
+    const songAdjectives = document.getElementById('input-song-adjectives').value;
+    const songMood = document.getElementById('input-song-mood').value;
+    const momentInLife = document.getElementById('textarea-moment-in-life').value;
+    const associations = document.getElementById('textarea-associations').value;
 
     // Time
-    songFavouriteMoments = document.getElementById('song-favourite-moments');
-    songHorribleMoments= document.getElementById('song-horrible-moments');
+    const songFavouriteMoments = document.getElementById('input-song-favourite-moments').value;
+    const songHorribleMoments= document.getElementById('input-song-horrible-moments').value;
 
     // More information
 
-    // Add the song?
+    const willAddYes = document.getElementById('input-will-add-yes').checked;    
+    const willAddNo = document.getElementById('input-will-add-no').checked;
     
-    songComments = document.getElementById('song-comments');
+    const songComments = document.getElementById('textarea-song-comments').value;
 
     // Doodle
+    const imageData = uploader.getImageData();
+    if (imageData) {
+        const placeUploadImage = document.getElementById('card-doodle');
+        placeUploadImage.src = imageData;
+        placeUploadImage.style.display = "block";
+    }
 
     console.log(
-        songUrl.value
+        songUrl + songName + bandName +
+        songAdjectives + songMood + 
+        momentInLife + associations +
+        songFavouriteMoments + songHorribleMoments +
+        songComments  
     );
+    console.log(willAddNo);
+    // var img = document.createElement('img');
+    // img.alt = "DOODLE";
+    // img.src = doodle;
+    // document.body.appendChild(img);
+}
 
+function setupFileUploader(fileInputElementId){
+    // Function that creates an EventListener for fileInputElement
+    let uploadedImageData = "";
+
+    const inputElement = document.getElementById(fileInputElementId);
+
+    inputElement.addEventListener('change', (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                uploadedImageData = e.target.result;
+            }
+            reader.readAsDataURL(file);
+        }
+    })
+    return {
+        getImageData: () => uploadedImageData,
+    };
 }
 
 document.addEventListener("DOMContentLoaded", function(event){
+
+    const uploader = setupFileUploader('input-image-doodle');
     
     buttonClear = document.getElementById('button-clear');
     buttonClear.addEventListener("click", clearAllFields);
 
     buttonSubmit = document.getElementById('button-submit');
-    buttonSubmit.addEventListener('click', actionButtonSubmit);
-    
+    buttonSubmit.addEventListener('click', () => {
+        actionButtonSubmit(uploader);
+    });
 });
 
